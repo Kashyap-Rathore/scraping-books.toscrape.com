e0a93589ac681fabf500d3b6b819d7f6ea8f6112
Kashyap Rathore <32009249+Kashyap-Rathore@users.noreply.github.com>
2020-01-25 12:41:11 +0530

    Add files via upload

---
+++ locator/BookLocator.py
@@ -0,0 +1,7 @@
+class BookLocator:
+    NAME= 'article.product_pod h3 a'
+    PRICE= 'article.product_pod div.product_price p.price_color'
+    RATING= 'article.product_pod p.star-rating'
+    LINK= 'article.product_pod h3 a'
+
+
---
+++ locator/BookPageLocator.py
@@ -0,0 +1,4 @@
+class BookPageLocator:
+    LOCATOR= 'div.page_inner section li.col-xs-6.col-sm-4.col-md-3.col-lg-3'
+    PAGE= 'div.page_inner div.row div.col-sm-8.col-md-9 section div div ul.pager li.current'
+
